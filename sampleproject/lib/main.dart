c3e0c959df54e363f04e5aaac3214d9b1dfae157
Saad575757 <smartysaad59@gmail.com>
2022-02-24 21:28:44 +0500

    chapter done

--- sampleproject/lib/main.dart
+++ sampleproject/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -18,31 +19,79 @@ class Homepage extends StatelessWidget {
         appBar: AppBar(
           title: Text("first App"),
         ),
-        body: Center(
+        body: Padding(padding: const EdgeInsets.all(20),
+         child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+          boxShadow: [BoxShadow(color: Colors.black, offset: Offset(2.0,5.0),blurRadius: 5),],
+          borderRadius: BorderRadius.circular(10)),
+
+        child: Column(
+          
+          
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            
+            
+            children: [
+              
+              
+              Container(
+                width: 100,
+                height:100,
+                color: Colors.red,
+                child: Center(child: Text("It's a box", style: TextStyle(fontSize: 20),),),
+               
+                
+                
+              
+
+
+              ),
+               Container(
+                width: 100,
+                height:100,
+                color: Colors.yellow,
+                child: Center(child: Text("It's a 2nd box")),
+                
+              ),
+               Container(
+                width: 100,
+                height:100,
+                color: Colors.green,
+                child: Center(child: Text("It's a 3rd box")),
+                
+              ),
+            ],
+          
           //child: Center(child: Text("Hi saad")),
 
-          child: Container(
-            padding:  const EdgeInsets.all(8),
-            alignment: Alignment.center,
+          // child: Container(
+          //   padding:  const EdgeInsets.all(8),
+          //   alignment: Alignment.center,
             
-            width: 200,
-            height: 200,
-            child: Text("I'm a box",textAlign: TextAlign.center, style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              fontSize: 30,  ) ), 
-            decoration: BoxDecoration(
-              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5, offset: Offset(2.0,5.0))],
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])
-            ),
-          ),
+          //   width: 200,
+          //   height: 200,
+          //   child: Text("I'm a box",textAlign: TextAlign.center, style: TextStyle(
+          //     color: Colors.white,
+          //     fontWeight: FontWeight.bold,
+          //     fontStyle: FontStyle.italic,
+          //     fontSize: 30,  ) ), 
+          //   decoration: BoxDecoration(
+          //     boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5, offset: Offset(2.0,5.0))],
+          //     borderRadius: BorderRadius.circular(10),
+          //     color: Colors.teal,
+          //     gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])
+          //   ),
+          // ),
 
           
           
         ),
+        ),
+        ),  
       );
 
 
